c56ecc5dc3a776a8ec512b4db15d4b3d6faf842a
Alexandre Machado <alexandrem16@al.insper.edu.br>
2024-04-19 14:41:46 -0300

    front lobby pronto

--- sardrones/src/paginas/cliente_lobby/cliente_lobby.js
+++ sardrones/src/paginas/cliente_lobby/cliente_lobby.js
@@ -7,36 +7,29 @@ function Lobby() {
         <div className="Lobby">
             <header className="App-header">
                 <div className="logo">
-                    <img src= "/logo_apenas.png"  alt="SARdrones Logo" />
-                    <img src= "/logoescrito.png"  alt="Logo escrito" />
+                    <img src= "/logo_apenas.png"  alt="SARdrones Logo"  className='logo_img1'/>
+                    <img src= "/logoescrito.png"  alt="Logo escrito"className='logo_img2' />
                     <p>Área do Cliente</p>
                 </div>
                 <div className="area-login">
                     <img src='/hamburger.png' alt='Hamburger'/>
-                    <img src='/perfil.png' alt='Perfil'/>
+                    <img src='/perfil.png' alt='Perfil' />
                 </div>
             </header>
 
-            <div className="lobby-container" style={{ backgroundImage: `url("/fundo.jpg")` }}>
-                <div className="background-image">
-                    {/* Conteúdo principal da página, por exemplo, links ou botões */}
-                </div>
+            <main className="lobby-container" style={{ backgroundImage: `url("/fundo.jpg")` }}>
                 <div className="trapezoid">
                     <div>
                         <img src='/logo_apenas.png' alt='logo main'/>
                         <h1>SARDRONES</h1>
                     </div>
                     <p>QUALIDADE ACIMA DE TUDO</p>
-                    {/* Outros textos ou elementos que você quer no trapézio */}
+                    <ul>
+                        <li>Seus Voos</li>
+                        <li>Análises</li>
+                    </ul>
                 </div>
-            </div>
-
-            <nav className="bottom-nav">
-                <ul>
-                    <li>Seus Voos</li>
-                    <li>Análises</li>
-                </ul>
-            </nav>
+            </main>
 
         </div>
     );
