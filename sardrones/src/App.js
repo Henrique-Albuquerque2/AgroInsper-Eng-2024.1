e32299eb4d6b67f4c9e9779dee26165ff1447662
victorfariasoares <victorfs2@al.insper.edu.br>
2024-04-23 19:48:35 -0300

    diversas modificacoes na pagina lobby e no header

--- sardrones/src/App.js
+++ sardrones/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Lobby from './paginas/cliente_lobby/cliente_lobby';
+import Header from './componentes/header';
 // Importar páginas !!!!!
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Header />
         <Routes>
           <Route path="/" element={<Lobby />} />
           {/* <Route path="/" element={<Voos />} /> */}
